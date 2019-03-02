f4e86df60b69d2ce72b732d2d26144e600712a4d
miyaokamarina <miyaokamarina@gmail.com>
2019-03-02 07:04:00 +0300

    feat(ui): Supported icons tinting.

--- @anireact/ui/src/Ui/Icon.tsx
+++ @anireact/ui/src/Ui/Icon.tsx
@@ -1,16 +1,19 @@
+import { bLuma, Color, gLuma, rLuma } from '@anireact/hsl';
 import { IconName, IconSize } from '@anireact/icons';
-import { px } from '@anireact/prelude';
+import { colorMatrix, px } from '@anireact/prelude';
 import React, { DetailedHTMLProps, HTMLAttributes } from 'react';
 import { decorate } from './decorate';
 
 export type Icon = DetailedHTMLProps<HTMLAttributes<HTMLImageElement>, HTMLImageElement> & {
     readonly name: IconName;
     readonly size?: IconSize;
+    readonly tintColor?: Color;
+    readonly tintStyle?: 'fill' | 'white' | 'black';
 };
 
 export const Icon = decorate<Icon, HTMLImageElement>(
     { name: 'Icon' },
-    ({ name, size = 16, ...props }, { ref, className, theme }) => {
+    ({ name, tintColor, tintStyle, size = 16, ...props }, { ref, className, theme }) => {
         const { iconsBase, iconsTheme } = theme;
 
         return (
@@ -29,9 +32,51 @@ export const Icon = decorate<Icon, HTMLImageElement>(
 
                         width: ${px(size)};
                         height: ${px(size)};
+
+                        filter: ${tint(tintColor, tintStyle)};
                     }
                 `}</style>
             </>
         );
     },
 );
+
+const tint = (tintColor?: Color, tintStyle = 'white' as 'fill' | 'white' | 'black') => {
+    if (tintColor) {
+        const rgbT = tintColor.rgba();
+        const [rT, gT, bT] = rgbT;
+
+        if (tintStyle === 'fill') {
+            // prettier-ignore
+            return colorMatrix(
+                0, 0, 0, 0, rT,
+                0, 0, 0, 0, gT,
+                0, 0, 0, 0, bT,
+                0, 0, 0, 1, 0,
+            );
+        }
+
+        if (tintStyle === 'black') {
+            // prettier-ignore
+            return colorMatrix(
+                (1 - rT) * rLuma, (1 - rT) * gLuma, (1 - rT) * bLuma, 0, rT,
+                (1 - gT) * rLuma, (1 - gT) * gLuma, (1 - gT) * bLuma, 0, gT,
+                (1 - bT) * rLuma, (1 - bT) * gLuma, (1 - bT) * bLuma, 0, bT,
+                0,                0,                0,                1, 0,
+            );
+        }
+
+        // prettier-ignore
+        return colorMatrix(
+            rT * rLuma, rT * gLuma, rT * bLuma, 0, 0,
+            gT * rLuma, gT * gLuma, gT * bLuma, 0, 0,
+            bT * rLuma, bT * gLuma, bT * bLuma, 0, 0,
+            0,          0,          0,          1, 0,
+        );
+
+        // TODO: Luma-preserving tinting: 0.2126 (1 - f x) + 0.7152 (1 - f x) + 0.0722 (1 - f x) + 0 + f x.
+        // TODO: Fill tinting:            0.2126 f x       + 0.7152 f x       + 0.0722 f x       + 0 + (-f x + x).
+    }
+
+    return 'initial';
+};
