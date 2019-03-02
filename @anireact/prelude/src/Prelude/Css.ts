a143a422637c326482f1ec1af3bac47913af9dcc
miyaokamarina <miyaokamarina@gmail.com>
2019-03-02 20:44:25 +0300

    feat(prelude): Added CSS class names helper.

--- @anireact/prelude/src/Prelude/Css.ts
+++ @anireact/prelude/src/Prelude/Css.ts
@@ -7,8 +7,11 @@ export const px = (x: number) => `${x}px`;
 
 export const cssvar = (name?: string) => `--${isNone(name) ? uuidv4() : `${uuidv4()}-${name}`}`;
 
-export const colorMatrix = (...matrix: Vector20<number>) =>
-    `url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg'><filter id='matrix'><feColorMatrix type='matrix' values='${map(
-        matrix,
-        x => x.toFixed(8),
-    ).join(' ')}' /></filter></svg>#matrix")`;
+export const colorMatrix = (...m: Vector20<number>) => [pu, ps, map(m, x => x.toFixed(8)).join(' '), ss, su].join('');
+
+const pu = `url("data:image/svg+xml;utf8,`;
+const ps = `<svg xmlns='http://www.w3.org/2000/svg'><filter id='matrix'><feColorMatrix type='matrix' values='`;
+const ss = ` /></filter></svg>`;
+const su = `#matrix")`;
+
+export const classes = (...classes: (string | null | undefined | void)[]) => classes.filter(Boolean).join(' ');
