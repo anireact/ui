a5c5688ce6fc3d05b076b3b32ead492cfa07e641
miyaokamarina <miyaokamarina@gmail.com>
2019-03-02 07:02:42 +0300

    feat(ui): Added hack to target decorated component from CSS.

--- @anireact/ui/src/Ui/decorate.tsx
+++ @anireact/ui/src/Ui/decorate.tsx
@@ -1,4 +1,4 @@
-import { px } from '@anireact/prelude';
+import { cssvar, isNone, not, px } from '@anireact/prelude';
 import { glowX, glowY, Level, LevelPalette, ThemedContext, useThemed } from '@anireact/themed';
 import { TldrContext, useTldr } from '@tld/r';
 
@@ -48,48 +48,44 @@ type Passed<P> = Pick<P, Exclude<keyof P, keyof CapturedProps>>;
 type Raw<P, R> = (props: Passed<P>, injected: InjectedProps<R>) => ReactNode;
 type Decorated<P, R> = FunctionComponent<Props<P, R>>;
 
-export const decorate = <P extends Required, R extends Element>(o: Decorator, c: Raw<P, R>): Decorated<P, R> => {
+export const decorate = <P extends Required, R extends HTMLElement>(o: Decorator, c: Raw<P, R>): Decorated<P, R> => {
     const { name, providesLevel, consumesLevel } = o;
+    const root = cssvar(name);
 
     const Component = memo(
         forwardRef<R, P & CapturedProps>((props, ref) => {
             const { className, level, disabled = false, ...passed } = props;
-
             const tld = useTldr();
             const themed = useThemed();
             const levelContext = useContext(LevelContext);
-
             const actualLevel = consumesLevel ? level || levelContext || 'base' : level || 'base';
 
             const node = c(
-                {
-                    ...passed,
-                    onPointerMove: forward(passed.onPointerMove, onPointerMove),
-                },
+                { ...passed, onPointerMove: forward(passed.onPointerMove, onPointerMove) },
                 {
                     ref,
-                    className,
                     tld,
                     disabled,
-                    theme: {
-                        ...themed,
-                        ...themed[disabled ? 'disabled' : actualLevel],
-                    },
+                    className: [className, root].filter(not(isNone)).join(' '),
+                    theme: { ...themed, ...themed[disabled ? 'disabled' : actualLevel] },
                 },
             ) as ReactElement;
 
-            if (providesLevel) {
-                return <LevelContext.Provider value={actualLevel}>{node}</LevelContext.Provider>;
-            }
+            if (providesLevel) return <LevelContext.Provider value={actualLevel}>{node}</LevelContext.Provider>;
 
             return node;
 
-            // TODO: Handle checked.
             // TODO: Handle disabled the right way.
         }),
     );
 
     Component.displayName = name;
+
+    const value = () => `.${root}`;
+
+    Reflect.defineProperty(Component, 'toString', { value });
+    Reflect.defineProperty(Component, 'valueOf', { value });
+    Reflect.defineProperty(Component, Symbol.toPrimitive, { value });
 
     return Component;
 };
